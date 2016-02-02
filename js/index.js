cf9143dd6d654d9e1705bd8c4941e4009e10ae72
cheqianxiao <oobakoyo@gmail.com>
2016-02-02 20:11:27 +0800

    Revert "add backtop"
    This reverts commit f301971969b09f56850f9907d9ee338e96e639a3.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 
   $(document).ready(function(){
-    // 导航条
   	$(window).scroll(
   		function(){
   			if($(window).scrollTop() > 200) 
@@ -8,8 +7,8 @@
   	      else{ $('nav').show();
         }
     })
-    //音乐播放
-    var au = document.getElementById('player');
+
+    var au=document.getElementById('player');
 
      
     $('.fa-play').on('click',function(){ 
@@ -27,31 +26,9 @@
       $('.fa-pause').hide();
       au.pause();
     })
-
-    // 小象喷水
-    var elephant = $("#elephant-backtop");
-    var springs = elephant.children();
-
-    elephant.hover(
-      function(){
-        springs[0].style.display = "block";
-        springs[0].classList.add("spring-left");
-        springs[1].style.display = "block";
-        springs[1].classList.add("spring-right");
-      },
-      function(){
-        springs[0].style.display = "none";
-        springs[0].classList.remove("spring-left");
-        springs[1].style.display = "none";
-        springs[1].classList.remove("spring-right");
-      });
-
-    //返回顶部
-    $(function(){
-       var backtop = new BackTop($('#elephant-backtop'),{ pos: 1000});
-    });
+     
   	
-})
+  })
 
 
 
